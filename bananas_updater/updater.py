ed48eac7347ada73db168163569b89d3a353e0b2
Tom Cizeron <tom.cizeron@edhec.com>
2023-05-21 17:36:29 +0200

    Job toutes les heures

--- bananas_updater/updater.py
+++ bananas_updater/updater.py
@@ -4,5 +4,5 @@ from bananas_updater import create_bananas
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(create_bananas.bananas_of_the_day, 'interval', minutes=4)
+    scheduler.add_job(create_bananas.bananas_of_the_day, 'interval', hours=1)
     scheduler.start()
