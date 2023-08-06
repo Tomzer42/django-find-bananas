9b0fdb26502d591b2511c84d6c3374f4f442c626
Tom Cizeron <tom.cizeron@edhec.com>
2023-08-06 15:53:38 +0200

    Translate in english

--- find_bananas/static/find_bananas/functions.js
+++ find_bananas/static/find_bananas/functions.js
@@ -28,8 +28,8 @@ const validRound1 = (nbBanana) => {
     document.getElementById("input1").style.display ='none';
     document.getElementById("result1").style.display ='block';
     let guess = document.getElementById('guess_number1');
-    resultat = "Différence de bananes : " + (parseInt(guess.value) - parseInt(nbBanana)).toString();
-    good_number = "Le juste nombre : " + nbBanana
+    resultat = "The banana gap : " + (parseInt(guess.value) - parseInt(nbBanana)).toString();
+    good_number = "The right number : " + nbBanana
     document.getElementById('text-result1').textContent = resultat;
     document.getElementById('text-banana1').textContent = good_number;
 }
@@ -39,8 +39,8 @@ const validRound2 = (nbBanana) => {
     document.getElementById("input2").style.display ='none';
     document.getElementById("result2").style.display ='block';
     let guess = document.getElementById('guess_number2');
-    resultat = "Différence de bananes : " + (parseInt(guess.value) - parseInt(nbBanana)).toString();
-    good_number = "Le juste nombre : " + nbBanana
+    resultat = "The banana gap : " + (parseInt(guess.value) - parseInt(nbBanana)).toString();
+    good_number = "The right number : " + nbBanana
     document.getElementById('text-result2').textContent = resultat;
     document.getElementById('text-banana2').textContent = good_number;
 }
@@ -56,10 +56,10 @@ const validRound3 = (nbBanana1, nbBanana2, nbBanana3) => {
     resultat2 = Math.abs(parseInt(guess2) - parseInt(nbBanana2));
     resultat3 = Math.abs(parseInt(guess3) - parseInt(nbBanana3));
     score = resultat1 + resultat2 + resultat3;
-    score_final = "Ton score final : " + score.toString() + " " + String.fromCodePoint( 127820 ) + String.fromCodePoint( 127820 ) + String.fromCodePoint( 127820 );
+    score_final = "Your final score : " + score.toString() + " " + String.fromCodePoint( 127820 ) + String.fromCodePoint( 127820 ) + String.fromCodePoint( 127820 );
     res3 = (parseInt(guess3) - parseInt(nbBanana3));
-    resultat = "Différence de bananes : " + res3.toString();
-    good_number = "Le juste nombre : " + nbBanana3
+    resultat = "The banana gap : " + res3.toString();
+    good_number = "The right number : " + nbBanana3
     document.getElementById('text-result3').textContent = resultat;
     document.getElementById('text-banana3').textContent = good_number;
     document.getElementById('score').textContent = score_final;
@@ -77,7 +77,7 @@ const copyText = (nbBanana1, nbBanana2, nbBanana3, guess1 = null, guess2 = null,
   resultat2 = Math.abs(parseInt(guess2) - parseInt(nbBanana2));
   resultat3 = Math.abs(parseInt(guess3) - parseInt(nbBanana3));
   score = resultat1 + resultat2 + resultat3;
-  const shareSentence = "FindBananas #1"
+  const shareSentence = "FindBananas #127"
     + "\n\u2022 Round 1 : " + resultat1.toString() + " " + String.fromCodePoint(127820)
     + "\n\u2022 Round 2 : " + resultat2.toString() + " " + String.fromCodePoint(127820)
     + "\n\u2022 Round 3 : " + resultat3.toString() + " " + String.fromCodePoint(127820)
